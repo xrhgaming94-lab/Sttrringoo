879fdee3d6618922aa816603dc6a4dbfc442f2bc
xrhgaming94-lab <xrhgaming94@gmail.com>
2026-03-27 18:57:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ async def json_to_proto(json_data: str, proto_message: Message) -> bytes:
 def get_account_credentials(region: str) -> str:
     r = region.upper()
     if r == "IND":
-        return "uid=4657633505&password=S_47O7O_BY_STAR_GMR_E65Q7"
+        return "uid=4668980064&password=STAR_NNMZL_BY_STAR_GMR_YH1KI"
     elif r in {"BR", "US", "SAC", "NA"}:
         return "uid=4664973977&password=S_Y0QTY_BY_STAR_GMR_NRETK"
     else:
